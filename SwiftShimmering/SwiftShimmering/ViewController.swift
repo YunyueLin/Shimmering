6a894c67628d160c1fd5fee47c875da276969ee2
林赟越 <497075549@qq.com>
2018-07-31 16:25:07 +0800

    refine code

--- SwiftShimmering/SwiftShimmering/ViewController.swift
+++ SwiftShimmering/SwiftShimmering/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        view.backgroundColor = UIColor.init(white: 0, alpha: 0.8)
         
         shimmeringView.backgroundColor = .black
         loadingLabel.textColor = .white
         shimmeringView.contentView = loadingLabel
         shimmeringView.shimmering = true
-        
         
         let shimmeringV2 = ShimmeringView.init(frame: CGRect(x: 0, y: 200, width: 200, height: 200))
         view.addSubview(shimmeringV2)
@@ -28,12 +25,10 @@ class ViewController: UIViewController {
         shimmeringV2.addSubview(label2)
         label2.textColor = .white
         label2.text = "使用代码添加"
-        
         shimmeringV2.contentView = label2
         shimmeringV2.shimmering = true
         
     }
-    
     
     @IBOutlet weak var shimmeringView: ShimmeringView!
     @IBOutlet weak var loadingLabel: UILabel!
